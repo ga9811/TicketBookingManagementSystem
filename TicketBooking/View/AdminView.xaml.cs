bbd501fed69cd5924b3f5997ffec2f7c6c8ac832
ga9811 <123421414+ga9811@users.noreply.github.com>
2023-08-01 18:44:37 -0400

    Add files via upload

--- TicketBooking/View/AdminView.xaml.cs
+++ TicketBooking/View/AdminView.xaml.cs
@@ -19,6 +19,11 @@ using System.Security.Cryptography;
 using System.Collections.ObjectModel;
 using System.Data;
 using TicketBookingSystemWPF.DAO;
+using ControlzEx.Standard;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Windows.Media.Animation;
+using System.Xml.Linq;
+
 
 namespace TicketBookingSystemWPF.View
 {
@@ -28,9 +33,11 @@ namespace TicketBookingSystemWPF.View
     public partial class AdminView : Window
     {
         private UserService userService;
-        private TrainInfoModel selectedTrain;
-
+        private BookingsInfoModel selectedTrain;
+        private BookingsInfoModel selectedBooking;
         private Dictionary<int, string> stationNameMap;
+        //IBasicDao<BookingsInfoModel> basicDao = new BasicDaoWrapper<BookingsInfoModel>();
+       
 
         public AdminView()
         {
@@ -147,7 +154,7 @@ namespace TicketBookingSystemWPF.View
         private void Searchbtn_Click(object sender, RoutedEventArgs e)
         {
             int trainId = int.Parse(TrainId_textbox.Text);
-            List<TrainInfoModel> trains = userService.SearchTrainByTrainId(trainId);
+            List<BookingsInfoModel> trains = userService.SearchTrainByTrainId(trainId);
             dataGrid.ItemsSource = trains;
             updateTrainId.Text = TrainId_textbox.Text;
 
@@ -157,9 +164,21 @@ namespace TicketBookingSystemWPF.View
 
         private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            selectedTrain = (TrainInfoModel)dataGrid.SelectedItem;
+            //selectedTrain = (TrainInfoModel)dataGrid.SelectedItem;
 
+            selectedBooking = dataGrid.SelectedItem as BookingsInfoModel;
+            if (selectedBooking == null)
+            {
+                return;
+            }
 
+            updateTrainId.Text = selectedBooking.train_Id.ToString();
+            depTextBox.Text = selectedBooking.depStation.ToString();
+            arrTextBox.Text = selectedBooking.arrStation.ToString();
+            depTimeTextBox.Text = selectedBooking.dep_time.ToString();
+            arrTimeTextBox.Text = selectedBooking.arr_time.ToString();
+            string seat = selectedBooking.seatName;
+            decimal price = selectedBooking.price;
 
 
 
@@ -167,22 +186,85 @@ namespace TicketBookingSystemWPF.View
         }
         private void TrainInfoUpdate()
         {
+           
 
         }
 
-        //private void Update_Click(object sender, RoutedEventArgs e)
-        //{
-        //    if (selectedTrain != null)
-        //    {
-        //        userService.UpdateTrain(selectedTrain);
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //using (var connection = new SqlConnection(@"Data Source=DESKTOP-D9VVUP6;Initial Catalog=TicketBooking;Integrated Security=True"))
+                {
+                    connection.Open();
+                    string sqlDelete = "DELETE FROM Trains WHERE train_Id = @p0";
+                    using (var command = new SqlCommand(sqlDelete, connection))
+                    {
+                        int trainId = int.Parse(updateTrainId.Text);
+                        command.Parameters.AddWithValue("@p0", trainId);
+                        command.ExecuteNonQuery();
+                    }
+                    string sqlInsert = "INSERT INTO Trains (train_Id, dep_id, arr_id, seat_Id, dep_time, arr_time, price) " +
+                   "VALUES (@p0, (SELECT station_Id FROM Stations WHERE station = @depStation), " +
+                   "(SELECT station_Id FROM Stations WHERE station = @arrStation), @seatId, @depTime, @arrTime, @price)";
 
-        //        MessageBox.Show("Train updated successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Please select a train to update.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-        //    }
-        //}
+                    using (var command = new SqlCommand(sqlInsert, connection))
+                    {
+                        int trainId = int.Parse(updateTrainId.Text);
+                        string depStation = depTextBox.Text.ToString();
+                        string arrStation = arrTextBox.Text.ToString();
+
+                        DateTime depTime = DateTime.Parse(depTimeTextBox.Text);
+                        DateTime arrTime = DateTime.Parse(arrTimeTextBox.Text);
+                        System.Diagnostics.Debug.WriteLine("depTime: " + depTime.ToString());
+                        System.Diagnostics.Debug.WriteLine("arrTime: " + arrTime.ToString());
+
+                        decimal price = 0;
+
+                        for (int seatId = 1; seatId <= 20; seatId++)
+                        {
+                            if (seatId <= 10)
+                                price = 50;
+                            else
+                                price = 80;
+
+                            command.Parameters.Clear();
+                            command.Parameters.AddWithValue("@p0", trainId);
+                            command.Parameters.AddWithValue("@depStation", depStation);
+                            command.Parameters.AddWithValue("@arrStation", arrStation);
+                            command.Parameters.AddWithValue("@depTime", depTime);
+                            command.Parameters.AddWithValue("@arrTime", arrTime);
+                            command.Parameters.AddWithValue("@seatId", seatId);
+                            command.Parameters.AddWithValue("@price", price);
+
+                            command.ExecuteNonQuery();
+                        }
+                        
+
+                    }
+                    int trainId1 = int.Parse(updateTrainId.Text);
+                    List<BookingsInfoModel> trains = userService.SearchTrainByTrainId(trainId1);
+                    dataGrid.ItemsSource = trains;
+
+                    updateTrainId.Text = "";
+                    depTextBox.Text = "";
+                    arrTextBox.Text = "";
+                    depTimeTextBox.Text = "";
+                    arrTimeTextBox.Text = "";
+                }
+                
+                
+
+
+                MessageBox.Show("Train updated successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to update train: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            connection.Close();
+        }
 
         private void userDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -211,33 +293,31 @@ namespace TicketBookingSystemWPF.View
 
         }
 
-        private void Update_Click(object sender, RoutedEventArgs e)
-        {
-            int updatedTrainId = int.Parse(updateTrainId.Text);
+        //private void Update_Click(object sender, RoutedEventArgs e)
+        //{
+        //    int updatedTrainId = int.Parse(updateTrainId.Text);
 
-            List<TrainInfoModel> trains = userService.SearchTrainByTrainId(int.Parse(TrainId_textbox.Text));
-
-
-
-            if (trains.Count == 0)
-            {
-                // 找不到符合条件的车次信息
-                return;
-            }
-
-            //更新所有符合条件的车次信息
-            foreach (TrainInfoModel train in trains)
-            {
-                train.train_Id = updatedTrainId;
-                Console.WriteLine(updatedTrainId);
-                userService.UpdateTrain(train);
-                Console.WriteLine("Train updated successfully!");
-            }
+        //    //List<BookingsInfoModel> trains = userService.SearchTrainByTrainId(int.Parse(TrainId_textbox.Text));
 
 
-            //显示成功的提示
-            MessageBox.Show("update successful");
-        }
+
+        //    //if (trains.Count == 0)
+        //    //{
+
+        //    //    return;
+        //    //}
+
+        //    //foreach (BookingsInfoModel train in trains)
+        //    //{
+        //    //    train.train_Id = updatedTrainId;
+        //    //    Console.WriteLine(updatedTrainId);
+        //    //    userService.UpdateTrain(train);
+        //    //    Console.WriteLine("Train updated successfully!");
+        //    //}
+
+        //    ////显示成功的提示
+        //    //MessageBox.Show("update successful");
+        //}
 
         private void populate()
         {
@@ -258,19 +338,19 @@ namespace TicketBookingSystemWPF.View
                 return;
             }
 
-            List<TrainInfoModel> trains = userService.SearchTrainByTrainId(trainId);
+            List<BookingsInfoModel> trains = userService.SearchTrainByTrainId(trainId);
 
-           
+
             int deletedCount = userService.DeleteTrainByTrainId(trainId);
 
             if (deletedCount > 0)
             {
-               
+
                 MessageBox.Show("Delete successful");
             }
             else
             {
-                
+
                 MessageBox.Show("Delete failed");
             }
         }
